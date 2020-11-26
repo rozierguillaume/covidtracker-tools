49440870c9484849e43a48230547e3c86965e339
Guillaume ROZIER <guillaume.rozier@telecomnancy.net>
2020-11-26 10:12:02 +0100

    CoviRisque v3 : updates

--- src/CoviDep/covidep_france.php
+++ src/CoviDep/covidep_france.php
@@ -1,5 +1,6 @@
 <!-- wp:html -->
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet"/>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
 <div class="entry">
     <h3>Comment se situe mon département par rapport à la France ?</h3>
     <div class="row">
