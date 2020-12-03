b4d73dc654c1179160490257bff3e283f3ed0b36
Guillaume ROZIER <guillaume.rozier@telecomnancy.net>
2020-12-03 15:49:42 +0100

    Correction typo

--- src/DashboardRegion/selectEtCarte.php
+++ src/DashboardRegion/selectEtCarte.php
@@ -7,7 +7,7 @@
             <li>évolution du taux d'occupation en réanimation.</li>
         </ul>
         Sélectionnez les régions que vous souhaitez consulter à l'aide du menu déroulant ci-dessous
-        ou en cliquant directement sur la carte de France pour les départements de métropole.
+        ou en cliquant directement sur la carte de France métropolitaine.
         </p>
         <div class="text-center">
             <select multiple="multiple" name="regions_list_choice" id="listeRegions" class="select2">
