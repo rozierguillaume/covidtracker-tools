716cbf684b24d8c0a72d68ffc721eabec955650f
Guillaume Rozier <28guillaume28@gmail.com>
2020-12-04 12:42:33 +0100

    [Dash Regions] Suppression bouton accueil

--- src/DashboardRegion/dashboard-region.php
+++ src/DashboardRegion/dashboard-region.php
@@ -1,9 +1,6 @@
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet"/>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
 
-<div class="wp-block-button is-style-outline"><a class="wp-block-button__link" href="https://covidtracker.fr">&lt;
-        Retour <b>Accueil</b></a></div>
-
 <?php include(dirname(__FILE__) . '/selectEtCarte.php') ?>
 
 <script id="regionTemplate" type="text/template">
