75301c083c6100a5dc8410be3566f7430a238773
Guillaume Rozier <guillaume.rozier@telecomnancy.net>
2021-01-14 20:37:55 +0100

    Update vaccintracker_map.php

--- src/VaccinTracker/vaccintracker_map.php
+++ src/VaccinTracker/vaccintracker_map.php
@@ -566,7 +566,7 @@ function fetchOtherData(){
       )
 
     // Get data from health ministry csv
-    fetch('https://www.data.gouv.fr/fr/datasets/r/b234a041-b5ea-4954-889b-67e64a25ce0d', {cache: 'no-cache'})
+    fetch('https://raw.githubusercontent.com/rozierguillaume/vaccintracker/main/data.csv', {cache: 'no-cache'}) //https://www.data.gouv.fr/fr/datasets/r/b234a041-b5ea-4954-889b-67e64a25ce0d
         .then(response => {
             if (!response.ok) {
                 throw new Error("HTTP error " + response.status);
