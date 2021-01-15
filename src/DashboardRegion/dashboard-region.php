9fbaa1bb3ac03fb64b2381f6ae4a2651ae376c70
Michael Souvy <michael@souvy-informatique.fr>
2021-01-15 21:36:31 +0100

    Fix page dashboard région.

--- src/DashboardRegion/dashboard-region.php
+++ src/DashboardRegion/dashboard-region.php
@@ -1,8 +1,8 @@
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet"/>
-<link href="dashboardRegion.css" rel="stylesheet"/>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
-<script src="dashboardRegion.js"></script>
 
+<?php include(dirname(__FILE__) . '/dashboardRegionJs.php') ?>
+<?php include(dirname(__FILE__) . '/dashboardRegionCss.php') ?>
 <?php include(dirname(__FILE__) . '/selectEtCarte.php') ?>
 <?php include(dirname(__FILE__) . '/templateRegion.php') ?>
 
