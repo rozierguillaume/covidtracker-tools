1a7d154b51735b88ee5f6139d58299e04707d037
rozierguillaume <guillaume.rozier73@gmail.com>
2021-02-05 10:58:32 +0100

    correctif

--- src/VaccinTracker/resume.php
+++ src/VaccinTracker/resume.php
@@ -3,14 +3,14 @@
 <div class="wrap">
     <div class="one">
         <span id="nb_doses_injectees" style="font-size:200%; margin-top:5px; margin-bottom: 3px;">--</span>&nbsp;&nbsp;(+<span id="nb_doses_injectees_24h">--</span> en 24h)<br>
-        <b>Première doses injectées</b><br>
+        <b>Premières doses injectées</b><br>
         Nombre cumulé de personnes ayant reçu une dose de vaccin. Deux doses sont nécessaires pour vacciner un patient.
         <div style="font-size: 70%; margin-top: 3px;"><i>Dernière donnée : <span id="date_maj_1">--/--</span>.<br>Source : CovidTracker/Ministère de la Santé.</i></div>
     </div>
 
     <div class="one">
         <span id="nb_vaccines_2_doses" style="font-size:200%; margin-top:5px; margin-bottom: 3px;">--</span>&nbsp;&nbsp;(+<span id="nb_vaccines_24h_2_doses">--</span> en 24h)<br>
-        <b>Deuxième doses injectées</b><br>
+        <b>Secondes doses injectées</b><br>
         Nombre cumulé de personnes ayant reçu les deux doses de vaccin.
         <div style="font-size: 70%; margin-top: 3px;"><i>Dernière donnée : <span id="date_maj_2">--/--</span>.<br>Source : CovidTracker/Ministère de la Santé.</i></div>
     </div>
