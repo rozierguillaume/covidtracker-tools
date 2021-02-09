f9d029082ffeb8aa8d87ab45588e9a559958e3ba
chabrulat <77581665+chabrulat@users.noreply.github.com>
2021-02-09 12:02:53 +0100

    Update autorisations.php

--- src/VaccinTracker/autorisations.php
+++ src/VaccinTracker/autorisations.php
@@ -1,16 +1,17 @@
 <h2 style="margin-top : 80px;" id="autorisation-des-vaccins">Autorisation des vaccins</h2>
 <div style="border: 0px solid grey; background-color: rgba(128, 128, 128, 0.1); border-radius: 7px; padding: 12px;">
-    L'Union Européenne a passé des commandes pour 6 vaccins différents. 3 sont autorisés, aucun n'est en cours d'autorisation et 3 n'ont pas encore été soumis. <i>Mise à jour : 29/01.</i><br><br>
+    L'Union Européenne a passé des commandes pour 6 vaccins différents. 3 sont autorisés, aucun n'est en cours d'autorisation et 3 n'ont pas encore été soumis. <i>Mise à jour : 02/02.</i><br><br>
     <h3 style="margin-top : 10px;">Vaccins autorisés</h3>
     <div class="row">
         <card class="shadow-btn-green col-xs-11 col-md-4"><b>🇺🇸🇩🇪 Pfizer-BioNTech</b><br>
             Type : ARN messager<br>
             Efficacité annoncée : 94.5%<br>
             Conservation : -70°C<br>
-            Commandes UE : 500M + option 100M <br>
+            Commandes UE : 500M + option 100M<br>
             Dont France : 75M + 15M<br>
             Doses à injecter : 2<br>
-            Statut : autorisé (UE, France)
+            Statut : autorisé (UE, France)<br>
+            Article : https://www.nejm.org/doi/full/10.1056/NEJMoa2034577
         </card>
 
         <card class="shadow-btn-green col-xs-11 col-md-4"><b>🇺🇸 Moderna</b><br>
@@ -20,7 +21,8 @@
             Commandes UE : 160M <br>
             Dont France : 24M<br>
             Doses à injecter : 2<br>
-            Statut : autorisé (UE, France)
+            Statut : autorisé (UE, France)<br>
+            Article : https://www.nejm.org/doi/full/10.1056/NEJMoa2035389
         </card>
         <card class="shadow-btn-green col-xs-11 col-md-4"><b>🇬🇧🇸🇪 AstraZeneca/Oxford</b><br>
             Phase 3/3<br>
@@ -29,7 +31,8 @@
             Conservation : +5°C (6 mois)<br>
             Commandes UE : 300M (+ option 100M)<br>
             Doses à injecter : 2<br>
-            Statut : autorisé UE (29/01)
+            Statut : autorisé (UE, France)<br>
+            Article : https://www.thelancet.com/journals/lancet/article/PIIS0140-6736(20)32661-1
         </card>
     </div>
 
