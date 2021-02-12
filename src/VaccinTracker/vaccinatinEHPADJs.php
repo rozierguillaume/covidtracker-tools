c183b0dba699351865ae31f114700701c647ba5f
Michael Souvy <michael@souvy-informatique.fr>
2021-02-12 11:53:32 +0100

    Ajout cartes vaccination EHPAD.

---
+++ src/VaccinTracker/vaccinatinEHPADJs.php
@@ -0,0 +1,139 @@
+<script>
+    Array.prototype.asortBy = function (p) {
+        return this.slice(0).sort(function (a, b) {
+            return (a[p] < b[p]) ? 1 : (a[p] > b[p]) ? -1 : 0;
+        });
+    };
+
+    Number.prototype.addZero = function () {
+        if (this <= 9) {
+            return "0" + this;
+        }
+        return this
+    };
+
+    var donneesDepartementsEhpadVaccination;
+
+
+    jQuery(document).ready(function ($) {
+
+        var tableauValeurs = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+
+        var tableauCouleurs1dose= [
+            "#cfdde6",
+            "#b8d4e6",
+            "#a1cbe6",
+            "#8ac2e6",
+            "#73bae6",
+            "#5cb1e6",
+            "#45a8e6",
+            "#2e9fe6",
+            "#1796e6",
+            "#0076bf"
+        ];
+
+        var tableauCouleurs2doses= [
+            "#fff7f7",
+            "#f6fbc6",
+            "#cbfb58",
+            "#7ae24a",
+            "#61c142",
+            "#56ab3d",
+            "#2e9c28",
+            "#1c7b21",
+            "#116522",
+            "#084004"
+        ];
+
+
+        fetch('https://raw.githubusercontent.com/rozierguillaume/vaccintracker/main/data/output/vacsi-res-tot-dep.json')
+            .then(response => {
+                console.log(response);
+                if (!response.ok) {
+                    throw new Error("HTTP error " + response.status);
+                }
+                return response.json();
+            })
+            .then(json => {
+                donneesDepartementsEhpadVaccination = json;
+                console.log(donneesDepartementsEhpadVaccination);
+                colorationsCartesEHPAD();
+            });
+
+        function colorationsCartesEHPAD() {
+            pourcentage = true;
+
+            $('#carteEHPAD1Dose path').css("fill", "#c4c4cb");
+            $('#carteEHPAD2Doses path').css("fill", "#c4c4cb");
+
+            construireLegendesEHPAD(tableauValeurs, tableauCouleurs1dose, $("#legendeCarteEHPAD1Dose"));
+            construireLegendesEHPAD(tableauValeurs, tableauCouleurs2doses, $("#legendeCarteEHPAD2Doses"));
+
+            for (numeroDepartement in donneesDepartementsEhpadVaccination) {
+                if (numeroDepartement == '00'){
+                    continue;
+                }
+                donneesDepartement = donneesDepartementsEhpadVaccination[numeroDepartement];
+
+                var departementCarte = $('#carteEHPAD1Dose path[data-num="' + numeroDepartement + '"]');
+                departementCarte.data("res_couv_tot_dose1", donneesDepartement["res_couv_tot_dose1"]);
+                departementCarte.css("fill", recupererCouleurEHPAD(donneesDepartement["res_couv_tot_dose1"], tableauValeurs, tableauCouleurs1dose));
+
+                var departementCarte = $('#carteEHPAD2Doses path[data-num="' + numeroDepartement + '"]');
+                departementCarte.data("res_couv_tot_dose2", donneesDepartement["res_couv_tot_dose2"]);
+                departementCarte.css("fill", recupererCouleurEHPAD(donneesDepartement["res_couv_tot_dose2"], tableauValeurs, tableauCouleurs2doses));
+            }
+        }
+
+        function recupererCouleurEHPAD(valeur, tableauDonnees, tableauCouleurs) {
+            for (i = tableauCouleurs.length - 1; i >= 0; i--) {
+                if (i == 0) {
+                    return tableauCouleurs[i];
+                } else if (valeur >= tableauDonnees[i]) {
+                    return tableauCouleurs[i];
+                }
+            }
+            return "#727272";
+        }
+
+        function construireLegendesEHPAD(values = [], colors = [], divLegende) {
+            content = $('#legendTemplatePre').html();
+            values.map((val, idx) => {
+                if (pourcentage && (val != '>')) {
+                    if (val > 0) {
+                        content += $('#legendTemplateMid').html().replaceAll("valeur", '+ ' + val + ' %').replaceAll("colorBg", colors[idx]);
+                    } else {
+                        content += $('#legendTemplateMid').html().replaceAll("valeur", val + ' %').replaceAll("colorBg", colors[idx]);
+                    }
+                } else {
+                    content += $('#legendTemplateMid').html().replaceAll("valeur", val).replaceAll("colorBg", colors[idx]);
+                }
+            });
+            content += $('#legendTemplatePost').html();
+            divLegende.html(content);
+        }
+
+
+        $('#carteEHPAD1Dose .departement path').hover(function (e) {
+            departement = $(this).data("num");
+            nomDepartement = $("#listeDepartements option[data-num='" + departement + "']").val();
+            if ($(this).data("res_couv_tot_dose1")){
+                $('#carteEHPAD1Dose #map title').text(nomDepartement + ' (' + $(this).data("res_couv_tot_dose1").toFixed(1) + ' %)');
+            } else {
+                $('#carteEHPAD1Dose #map title').text(nomDepartement + ' : pas de donnée !');
+            }
+
+        });
+
+        $('#carteEHPAD2Doses .departement path').hover(function (e) {
+            departement = $(this).data("num");
+            nomDepartement = $("#listeDepartements option[data-num='" + departement + "']").val();
+            if ($(this).data("res_couv_tot_dose2")){
+                $('#carteEHPAD1Dose #map title').text(nomDepartement + ' (' + $(this).data("res_couv_tot_dose2").toFixed(1) + ' %)');
+            } else {
+                $('#carteEHPAD1Dose #map title').text(nomDepartement + ' : pas de donnée !');
+            }
+        });
+
+    });
+</script>
---
+++ src/VaccinTracker/vaccinationEHPAD.php
@@ -0,0 +1,37 @@
+<h2 style="margin-top : 80px;">
+    Vaccination en EHPAD
+</h2>
+<?php include(dirname(__FILE__) . '/vaccinatinEHPADJs.php') ?>
+
+<div class="" style="margin-bottom: 40px;">
+    Coloration en fonction du pourcentage de personnes hébergées en EPHAD vaccinée.
+    Données fournies par le Ministère de la Santé.
+</div>
+<br>
+
+<div id="menu" class="row">
+    <div class="col-md-6 text-center">
+        <div class="row">
+            <h4 id="titre_map" class="" style="display: inline-block">Proportion de résidents ayant reçu une dose :</h4>
+            <div class="col-xs-10">
+                <div id="carteEHPAD1Dose" style="margin-top:20px;">
+                    <?php include(__DIR__ . '/../DashboardDepartement/carteDepartements.php'); ?>
+                </div>
+            </div>
+            <div id="legendeCarteEHPAD1Dose" class="col-xs-2">
+
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6  text-center">
+        <h4 id="titre_map" class="" style="display: inline-block">Proportion de résidents ayant reçu deux doses :</h4>
+        <div class="col-xs-10">
+            <div id="carteEHPAD2Doses" style="margin-top:20px;">
+                <?php include(__DIR__ . '/../DashboardDepartement/carteDepartements.php'); ?>
+            </div>
+        </div>
+        <div id="legendeCarteEHPAD2Doses" class="col-xs-2">
+
+        </div>
+    </div>
+</div>
