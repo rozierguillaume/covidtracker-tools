5f31d93a9a9d420a28772e746c1e2a6ef66276ee
rozierguillaume <guillaume.rozier73@gmail.com>
2021-03-19 19:10:08 +0100

    update

---
+++ src/message_erreur.php
@@ -0,0 +1,6 @@
+<div class="alert alert-warning" role="alert">
+    <i>19 mars à 19h • </i>
+    <b>Pas de mise à jour des données</b><br>
+    Santé publique France n'a pas publié les données Covid19 du jour en raison d'un problème technique, CovidTracker sera mis à jour dès que cela sera le cas.
+    
+<br></div>
