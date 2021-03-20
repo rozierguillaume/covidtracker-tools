39c206caad3b456c67891ea3d3a8976f2b376ad1
rozierguillaume <guillaume.rozier73@gmail.com>
2021-03-20 19:42:50 +0100

    updates

--- src/message_erreur.php
+++ src/message_erreur.php
@@ -1,8 +1,8 @@
 <div class="alert alert-warning" role="alert">
     <i>20 mars à 18h • </i>
-    <b>Pas de mise à jour des données depuis jeudi</b><br>
-    Les données SI-DEP (nombre de tests, nombre de cas positifs, taux d’incidence, taux de positivité, etc), SI-VIC (nombre de patients à l’hôpital, nombre d’admissions en réanimation, etc) et SI-VAC (nombre de personnes vaccinées, couverture vaccinale en Ehpad, etc) n’ont pas été mises à jour depuis jeudi soir en raison de plusieurs incidents techniques sur les plateformes de Santé publique France.
-    CovidTracker sera mis à jour dès la publication de nouvelles données, probablement lundi. Informations sur <a href="https://www.leparisien.fr/societe/sante/covid-19-pourquoi-les-indicateurs-francais-de-lepidemie-sont-tres-perturbes-20-03-2021-O7ZUZGTZPNALXB67A5CLTZIVOA.php">Le Parisien</a>, 
+    <b>Données partielles depuis jeudi</b><br>
+    Depuis jeudi soir en raison de plusieurs incidents techniques sur les plateformes de Santé publique France, seules les données hospitalières sont à jour.
+    CovidTracker sera mis à jour dès la publication de nouvelles données, probablement d'ici lundi. Informations sur <a href="https://www.leparisien.fr/societe/sante/covid-19-pourquoi-les-indicateurs-francais-de-lepidemie-sont-tres-perturbes-20-03-2021-O7ZUZGTZPNALXB67A5CLTZIVOA.php">Le Parisien</a>, 
     <a href="https://twitter.com/guillaumerozier/status/1372969967570841601?s=21">suivi sur Twitter</a>.<br><br>
     <b>Nous enverrons une newsetter lorsque les données seront de nouveau mises à jour : <a href="https://covidtracker.fr/newsletter">abonnez-vous</a>.</b>
 </div>
