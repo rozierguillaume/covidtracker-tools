02d13b48f5350f144ecf04e57d9f65ab053bc3d9
rozierguillaume <guillaume.rozier73@gmail.com>
2021-03-22 14:05:20 +0100

    VaccinTracker : livraisons

--- src/VaccinTracker/typesVaccins.php
+++ src/VaccinTracker/typesVaccins.php
@@ -1,3 +1,5 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-annotation/0.5.7/chartjs-plugin-annotation.min.js" integrity="sha512-9hzM/Gfa9KP1hSBlq3/zyNF/dfbcjAYwUTBWYX+xi8fzfAPHL3ILwS1ci0CTVeuXTGkRAWgRMZZwtSNV7P+nfw==" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-annotation/0.5.7/chartjs-plugin-annotation.js" integrity="sha512-o2vKsxnxl66o5bM3x+Z4jvweDAM7DMShyXHQ5C8bWsm2aOJKmJl1V5JtRjoZpHJfdas3VPpzXngtikzo7u4rcw==" crossorigin="anonymous"></script>
 
 <h2 id="types-vaccins" style="margin-top : 80px;">Par type de vaccin</h2>
 <div class="row">
@@ -23,6 +25,12 @@
     </div>
 </div>
 
+<h3 style="margin-top: 40px;">Livraisons</h3>
+Livraisons passées ou officiellement prévues pour les prochaines semaines par type de vaccin.
+<div class="chart-container" style="position: relative; ">
+        <canvas id="lineChartTypesVaccinsLivraisons" style="margin-top:20px; max-height: 700px; max-width: 900px;"></canvas>
+</div>
+
 <script>
 var typesVaccins;
 var typesVaccinsLivraisons;
@@ -41,6 +49,8 @@ function fetchTypesVaccins(){
                 console.log(typesVaccins)
                 buildChartTypesVaccins();
                 buildLineTypeChart_tous();
+                nextFetch();
+                
                 
             })
             .catch(function () {
@@ -48,8 +58,10 @@ function fetchTypesVaccins(){
                     console.log("error-types")
                 }
             )
+}
 
-            fetch('https://raw.githubusercontent.com/rozierguillaume/vaccintracker/main/data/output/flux-tot-nat.json', {cache: 'no-cache'})
+function nextFetch(){
+    fetch('https://raw.githubusercontent.com/rozierguillaume/vaccintracker/main/data/output/flux-tot-nat.json', {cache: 'no-cache'})
             .then(response => {
                 if (!response.ok) {
                     throw new Error("HTTP error " + response.status);
@@ -58,9 +70,7 @@ function fetchTypesVaccins(){
             })
             .then(json => {
                 this.typesVaccinsLivraisons = json;
-                console.log("yay")
-                console.log(typesVaccinsLivraisons)
-                
+                buildLineTypeChartLivraisons();
             })
             .catch(function () {
                     this.dataError = true;
@@ -68,6 +78,7 @@ function fetchTypesVaccins(){
                 }
             )
 }
+
 
 var barChartTypesVaccins;
 var colors=["#1796e6", "#ef9a9a", "#B39DDB"]
@@ -153,6 +164,7 @@ function buildChartTypesVaccins(){
                 annotation: {
                 events: ["click"],
                 annotations: [
+                    
                 ]
             }
             }
@@ -277,6 +289,119 @@ function buildChartTypesVaccins(){
     function fermerPanneauTypes(){
         this.lineChartTypeVaccin.destroy()
         buildLineTypeChart_tous()
+    }
+
+    var lineChartTypeVaccinLivraisons;
+    function buildLineTypeChartLivraisons(){
+        
+        var ctx = document.getElementById('lineChartTypesVaccinsLivraisons').getContext('2d');
+        
+        this.lineChartTypeVaccinLivraisons = new Chart(ctx, {
+            type: 'line',
+            data: {
+                //labels: labels,
+                datasets: [
+                    {
+                        yAxisID:"livraisons",
+                        label: typesVaccins.noms_vaccins[1-1] + " ",
+                        data: typesVaccinsLivraisons[1].jour.map((day, idx) => ({x: day, y: typesVaccinsLivraisons[1].nb_doses_tot_cumsum[idx]})),
+                        borderWidth: 4,
+                        fill: true,
+                        backgroundColor: colors[1-1],
+                        borderColor: colors[1-1],
+                        pointRadius: 0,
+                        pointHitRadius: 10,
+                    },
+                    {
+                        yAxisID:"livraisons",
+                        label: typesVaccins.noms_vaccins[2-1] + " ",
+                        data: typesVaccinsLivraisons[2].jour.map((day, idx) => ({x: day, y: typesVaccinsLivraisons[2].nb_doses_tot_cumsum[idx]})),
+                        borderWidth: 4,
+                        fill: true,
+                        backgroundColor: colors[2-1],
+                        borderColor: colors[2-1],
+                        pointRadius: 0,
+                        pointHitRadius: 10,
+                    },
+                    {
+                        yAxisID:"livraisons",
+                        label: typesVaccins.noms_vaccins[3-1] + " ",
+                        data: typesVaccinsLivraisons[3].jour.map((day, idx) => ({x: day, y: typesVaccinsLivraisons[3].nb_doses_tot_cumsum[idx]})),
+                        borderWidth: 4,
+                        fill: true,
+                        backgroundColor: colors[3-1],
+                        borderColor: colors[3-1],
+                        pointRadius: 0,
+                        pointHitRadius: 10,
+                    },
+                ]
+            },
+            options: {
+                aspectRatio: 0.7,
+                maintainAspectRatio: true,
+                tooltips: {
+                    callbacks: {
+                        label: function(tooltipItem, data) {
+                            let value = data['datasets'][tooltipItem.datasetIndex]['data'][tooltipItem['index']].y.toString().split(/(?=(?:...)*$)/).join(' ');
+                            return data['datasets'][tooltipItem.datasetIndex]['label'] + ': ' + value.toString();
+                        }
+                    }
+                },
+                maintainAspectRatio: false,
+                plugins: {
+                    deferred: {
+                        xOffset: 150,   // defer until 150px of the canvas width are inside the viewport
+                        yOffset: '50%', // defer until 50% of the canvas height are inside the viewport
+                        delay: 200      // delay of 500 ms after the canvas is considered inside the viewport
+                    }
+                },
+                scales: {
+                    yAxes: [{
+                        id:"livraisons",
+                        stacked: true,
+                        gridLines: {
+                            display: false
+                        },
+                        ticks: {
+                            callback: function (value) {
+                                        return value/1000 +" k";
+                                    }
+                        }
+                    }],
+                    xAxes: [{
+                        //stacked: true,
+                        ticks:{
+                            source: 'auto'
+                        },
+                        type: 'time',
+                        distribution: 'linear',
+                        gridLines: {
+                            display: false
+                        }
+                    }]
+                },
+                annotation: {
+                    events: ["click"],
+                    annotations: [
+                        {
+                            drawTime: "afterDatasetsDraw",
+                            type: "line",
+                            mode: "vertical",
+                            scaleID: "x-axis-0",
+                            value: moment(),
+                            borderWidth: 2,
+                            borderColor: "black",
+                            label: {
+                                content: "Aujourd'hui",
+                                enabled: true,
+                                position: "top"
+                            }
+                        }
+                    ]
+                }
+            }
+        });
+
     }
 
     function buildLineTypeChart_tous(){
