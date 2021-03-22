a4eff580ead59b1e2b2ef48c2d647514715d33d2
rozierguillaume <guillaume.rozier73@gmail.com>
2021-03-22 18:00:51 +0100

    Alert

--- src/message_erreur.php
+++ src/message_erreur.php
@@ -1,8 +1,7 @@
-<div class="alert alert-warning" role="alert">
-    <i>20 mars à 18h • </i>
-    <b>Données partielles depuis jeudi</b><br>
-    Depuis jeudi soir en raison de plusieurs incidents techniques sur les plateformes de Santé publique France, seules les données hospitalières sont à jour.
-    CovidTracker sera mis à jour dès la publication de nouvelles données, probablement d'ici lundi. Informations sur <a href="https://www.leparisien.fr/societe/sante/covid-19-pourquoi-les-indicateurs-francais-de-lepidemie-sont-tres-perturbes-20-03-2021-O7ZUZGTZPNALXB67A5CLTZIVOA.php">Le Parisien</a>, 
+<div class="alert alert-success" role="alert">
+    <i>22 mars à 18h • </i>
+    <b>✅ Données à jour</b><br>
+    Les problèmes de remontées de données à Santé publique France ont été corrigés, CovidTracker est désormais de nouveau à jour. Informations sur <a href="https://www.leparisien.fr/societe/sante/covid-19-pourquoi-les-indicateurs-francais-de-lepidemie-sont-tres-perturbes-20-03-2021-O7ZUZGTZPNALXB67A5CLTZIVOA.php">Le Parisien</a>, 
     <a href="https://twitter.com/guillaumerozier/status/1372969967570841601?s=21">suivi sur Twitter</a>.<br><br>
     <b>Nous enverrons une newsetter lorsque les données seront de nouveau mises à jour : <a href="https://covidtracker.fr/newsletter">abonnez-vous</a>.</b>
 </div>
