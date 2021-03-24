2b33b26f464983869c37d796fbed644e8344f64e
rozierguillaume <guillaume.rozier73@gmail.com>
2021-03-24 19:40:00 +0100

    Màj message spf

--- src/message_erreur.php
+++ src/message_erreur.php
@@ -1,7 +1,7 @@
 <div class="alert alert-danger" role="alert">
     <i>24 mars à 19h30 • </i>
     <b>Données non mises à jour</b><br>
-    Aucune donnée n'a été publiée en Open Data par Santé publique France aujourd'hui. Informations à venir. 
-    <a href="https://twitter.com/guillaumerozier/status/1374792212505108481?s=21">suivi sur Twitter</a>.<br>
+    Aucune donnée n'a été publiée en Open Data par Santé publique France aujourd'hui, les informations du site sont donc celles du 23 mars. CovidTracker sera automatiquement mis à jour dès publication de nouvelles données. 
+    <a href="https://twitter.com/guillaumerozier/status/1374792212505108481?s=21">Suivi sur Twitter</a>.<br>
     <b><a href="https://covidtracker.fr/newsletter">Abonnez-vous à la newsletter</a></b>
 </div>
