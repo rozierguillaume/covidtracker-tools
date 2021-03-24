11b42f2c2fb1330003ee6b6da69f3a6b8f93541c
rozierguillaume <guillaume.rozier73@gmail.com>
2021-03-24 19:38:40 +0100

    spf error

--- src/message_erreur.php
+++ src/message_erreur.php
@@ -1,7 +1,7 @@
-<div class="alert alert-success" role="alert">
-    <i>22 mars à 18h • </i>
-    <b>✅ Données à jour</b><br>
-    Les problèmes de remontées de données à Santé publique France ont été corrigés, CovidTracker est désormais de nouveau à jour. Informations sur <a href="https://www.leparisien.fr/societe/sante/covid-19-pourquoi-les-indicateurs-francais-de-lepidemie-sont-tres-perturbes-20-03-2021-O7ZUZGTZPNALXB67A5CLTZIVOA.php">Le Parisien</a>, 
-    <a href="https://twitter.com/guillaumerozier/status/1372969967570841601?s=21">suivi sur Twitter</a>.<br><br>
-    <b>Nous enverrons une newsetter lorsque les données seront de nouveau mises à jour : <a href="https://covidtracker.fr/newsletter">abonnez-vous</a>.</b>
+<div class="alert alert-danger" role="alert">
+    <i>24 mars à 19h30 • </i>
+    <b>Données non mises à jour</b><br>
+    Aucune donnée n'a été publiée en Open Data par Santé publique France aujourd'hui. Informations à venir. 
+    <a href="https://twitter.com/guillaumerozier/status/1374792212505108481?s=21">suivi sur Twitter</a>.<br>
+    <b><a href="https://covidtracker.fr/newsletter">Abonnez-vous à la newsletter</a></b>
 </div>
