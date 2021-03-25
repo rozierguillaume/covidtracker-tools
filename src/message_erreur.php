0f80439470de18605754a870637109fb36f3acf6
rozierguillaume <guillaume.rozier73@gmail.com>
2021-03-25 18:57:41 +0100

    update

---
+++ src/message_erreur.php
@@ -0,0 +1,6 @@
+<div class="alert alert-alert" role="alert">
+    <i>25 mars à 19h • </i>
+    <b>Données non mises à jour</b> : Santé publique France n'a pas publié les données du 25 mars pour le moment, CovidTracker n'est donc pas mis à jour.
+    <a href="https://twitter.com/guillaumerozier/status/1375138179947376640?s=21">Suivi sur Twitter</a>.<br>
+    <b><a href="https://covidtracker.fr/newsletter">Abonnez-vous à la newsletter</a></b>
+</div>
