61f101d4869cc2340a802e2adc4af8e45a746277
rozierguillaume <guillaume.rozier73@gmail.com>
2021-03-31 22:22:25 +0200

    update

--- src/message_erreur.php
+++ src/message_erreur.php
@@ -1,4 +1,4 @@
-<div class="alert alert-alert" role="alert">
-    <i>31 mars à 22h •  Données vaccinations</i>
+<div class="alert alert-warning" role="alert">
+    <i>31 mars à 22h •</i><b>  Données vaccination</b><br>
     Santé publique France indique : "Les indicateurs issus de la base Vaccin Covid n’ont pas pu être produits ce jour en raison d’une indisponibilité des données transmises."
 </div>
