f342f908fa46346194fbbb7283d4029c5a94fbd3
rozierguillaume <guillaume.rozier73@gmail.com>
2021-03-31 22:20:49 +0200

    alert

--- src/message_erreur.php
+++ src/message_erreur.php
@@ -1,7 +1,4 @@
-<!--<div class="alert alert-alert" role="alert">
-    <i>25 mars à 19h • </i>
-    <b>Données non mises à jour</b> : Santé publique France n'a pas publié les données du 25 mars pour le moment, CovidTracker n'est donc pas mis à jour.
-    <a href="https://twitter.com/guillaumerozier/status/1375138179947376640?s=21">Suivi sur Twitter</a>.<br>
-    <b><a href="https://covidtracker.fr/newsletter">Abonnez-vous à la newsletter</a></b>
+<div class="alert alert-alert" role="alert">
+    <i>31 mars à 22h •  Données vaccinations</i>
+    Santé publique France indique : "Les indicateurs issus de la base Vaccin Covid n’ont pas pu être produits ce jour en raison d’une indisponibilité des données transmises."
 </div>
--->
