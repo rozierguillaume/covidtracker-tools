d024c880e9a5b0a406c2732049fee11728c5cf36
rozierguillaume <guillaume.rozier73@gmail.com>
2021-04-14 20:24:50 +0200

    message d'erreur

--- src/message_erreur.php
+++ src/message_erreur.php
@@ -1,4 +1,5 @@
-<div class="alert alert-warning" role="alert">
+<!--<div class="alert alert-warning" role="alert">
     <i>13 avril à 22h •</i><b>  Données incomplètes</b><br>
     En raison de la publication de données incomplètes par Santé publique France, CovidTracker n'a pas pu être mis à jour le 13 avril. Il sera de nouveau à jour lors de la publication de l'ensemble des données.
 </div>
+-->
