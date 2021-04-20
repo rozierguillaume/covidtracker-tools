eca21e43b771b7c03107127f9a6df64ddd95a537
rozierguillaume <guillaume.rozier@telecomnancy.net>
2021-04-20 21:18:39 +0200

    message

--- src/message_erreur.php
+++ src/message_erreur.php
@@ -1,4 +1,4 @@
-<div class="alert alert-warning" role="alert">
-    <i>20 avril à 21h •</i><b>  Données incomplètes</b><br>
-    En raison d'un changement dans le format des données de Santé publique France, une partie de CovidTracker n'est pas à jour ce soir.
+<div class="alert alert-alert" role="alert">
+    <i>20 avril à 21h •</i><b>  Données erronées</b><br>
+    Santé publique France a publié ce soir des données contenant de nombreuses erreurs et problèmes de format. Nous nous excusons des bugs observés sur CovidTracker. Nous les avons contactés pour leur demander une résolution rapide.
 </div>
