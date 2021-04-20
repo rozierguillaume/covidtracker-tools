e0253a3f4c8e397e3452edb4a9bd57564deabd41
rozierguillaume <guillaume.rozier@telecomnancy.net>
2021-04-20 21:19:58 +0200

    danger

--- src/message_erreur.php
+++ src/message_erreur.php
@@ -1,4 +1,4 @@
-<div class="alert alert-warning" role="alert">
+<div class="alert alert-danger" role="alert">
     <i>20 avril à 21h •</i><b>  Données erronées</b><br>
     Santé publique France a publié ce soir des données contenant de nombreuses erreurs et problèmes de format. Nous nous excusons des bugs observés sur CovidTracker. Nous les avons contactés pour leur demander une résolution rapide.
 </div>
