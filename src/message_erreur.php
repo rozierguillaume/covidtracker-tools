9f9a061412ddc2eff1252f4629e9238640292f02
rozierguillaume <guillaume.rozier@telecomnancy.net>
2021-04-20 21:13:16 +0200

    message erreur

--- src/message_erreur.php
+++ src/message_erreur.php
@@ -1,5 +1,4 @@
-<!--<div class="alert alert-warning" role="alert">
-    <i>13 avril à 22h •</i><b>  Données incomplètes</b><br>
-    En raison de la publication de données incomplètes par Santé publique France, CovidTracker n'a pas pu être mis à jour le 13 avril. Il sera de nouveau à jour lors de la publication de l'ensemble des données.
+<div class="alert alert-warning" role="alert">
+    <i>20 avril à 21h •</i><b>  Données incomplètes</b><br>
+    En raison d'un changement dans le format des données de Santé publique France, une partie de CovidTracker n'est pas à jour ce soir.
 </div>
--->
