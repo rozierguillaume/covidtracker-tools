064e480f30907a1b80a485e4df069289cd820812
Bruno Spyckerelle <bruno@spyckerelle.fr>
2021-04-27 15:15:29 +0200

    correction libellé des résumés

--- src/VaccinTracker/resume.php
+++ src/VaccinTracker/resume.php
@@ -4,21 +4,21 @@
     <div class="one">
         <span id="nb_doses_injectees" style="font-size:200%; margin-top:5px; margin-bottom: 3px;">--</span>&nbsp;&nbsp;(+<span id="nb_doses_injectees_24h">--</span> en 24h)<br>
         <b>Premières doses injectées</b><br>
-        Nombre cumulé de personnes ayant reçu une dose de vaccin. Deux doses sont nécessaires pour vacciner un patient.
+        Nombre cumulé de personnes ayant reçu au moins une dose de vaccin.
         <div style="font-size: 70%; margin-top: 3px;"><i>Dernière donnée (J-1) : <span id="date_maj_1">--/--</span>.<br>Source : CovidTracker/Ministère de la Santé.</i></div>
     </div>
 
     <div class="one">
         <span id="nb_vaccines_totalement" style="font-size:200%; margin-top:5px; margin-bottom: 3px;">--</span>&nbsp;&nbsp;(+<span id="nb_vaccines_24h_totalement">--</span> en 24h)<br>
-        <b>Secondes doses injectées</b><br>
-        Nombre cumulé de personnes ayant reçu les deux doses de vaccin.
+        <b>Personnes vaccinées</b><br>
+        Nombre cumulé de personnes ayant reçu toutes les doses de vaccin (1, 2 ou 3 en fonction des vaccins et des personnes).
         <div style="font-size: 70%; margin-top: 3px;"><i>Dernière donnée (J-1) : <span id="date_maj_2">--/--</span>.<br>Source : CovidTracker/Ministère de la Santé.</i></div>
     </div>
 
     <div class="one">
         <span id="nb_doses" style="font-size:200%; margin-top:5px; margin-bottom: 3px;">--</span><br>
         <b>Doses réceptionnées</b><br>
-        Cumul des doses réceptionnées ou officiellement attendues depuis le 26 décembre. Deux doses espacées de trois semaines sont nécessaires pour vacciner un patient.<br>
+        Cumul des doses réceptionnées ou officiellement attendues depuis le 26 décembre.<br>
         <div style="font-size: 70%; margin-top: 3px;"><i>Dernière donnée* : <span id="date_maj_4">--/--</span>.<br>Source : CovidTracker/Ministère de la Santé.</i></div>
     </div>
 </div>
