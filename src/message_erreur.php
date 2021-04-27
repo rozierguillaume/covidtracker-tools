cfb2fd657fa752f984fc7020d6d22ed0cfb6f073
rozierguillaume <guillaume.rozier@telecomnancy.net>
2021-04-27 22:08:42 +0200

    retrait message

--- src/message_erreur.php
+++ src/message_erreur.php
@@ -1,4 +1,4 @@
-<div class="alert alert-warning" role="alert">
+<!--<div class="alert alert-warning" role="alert">
     <i>26 avril à 21h •</i><b> Données vaccination</b><br>
     En raison d'un changement de format dans les données fournies par Santé publique France, VaccinTracker n'est pas à jour. Nous travaillons sur un correctif.
-</div>
+</div>-->
