285658c9bdc1cbcd4f20f106b1836292b9a32e01
rozierguillaume <guillaume.rozier@telecomnancy.net>
2021-07-26 22:17:05 +0200

    message alerte

--- src/message_erreur.php
+++ src/message_erreur.php
@@ -1,6 +1,4 @@
-<!--
-    <div class="alert alert-warning" role="alert">
-    <i>25 juin •</i><b> Données vaccination</b><br>
-    Les données de vaccination ne sont plus publiées par Santé publique France depuis le 22 juin en raison de consolidations. VaccinTracker sera à jour dès que ces données seront de nouveau disponibles.
+<div class="alert alert-warning" role="alert">
+    <i>26 juillet •</i><b> Données non publiées</b><br>
+    Les données n'ont pas été publiées par Santé publique France ce jour. CovidTracker sera à jour dès que ces données seront de nouveau disponibles.
 </div>
--->
