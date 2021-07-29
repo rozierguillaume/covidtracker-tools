bb37f14e17db0b98f0d2520960994bb91565248d
rozierguillaume <guillaume.rozier@telecomnancy.net>
2021-07-29 20:07:15 +0200

    Suppression message erreur

--- src/message_erreur.php
+++ src/message_erreur.php
@@ -1,4 +1,4 @@
-<div class="alert alert-warning" role="alert">
+<!--<div class="alert alert-warning" role="alert">
     <i>28 juillet •</i><b> Données vaccination non publiées</b><br>
     Pour des raisons techniques, les données de vaccination n’ont pas été communiquées à Santé publique France ce jour. Les indicateurs ne peuvent donc pas être produits. CovidTracker sera à jour dès que ces données seront de nouveau disponibles.
-</div>
+</div>-->
