e9d7a966ab3e5921dff19ea2f18fbe6d00c7a7f0
rozierguillaume <guillaume.rozier@telecomnancy.net>
2021-09-24 21:16:45 +0200

    removing message

--- src/message_erreur.php
+++ src/message_erreur.php
@@ -1,4 +1,5 @@
-<div class="alert alert-warning" role="alert">
+<!--<div class="alert alert-warning" role="alert">
     <i>20 septembre •</i><b> Données vaccination non publiées</b><br>
     Santé publique France indique : "<i>Suite à des incohérences rencontrées sur les données de vaccination depuis le 15 septembre, les indicateurs ne sont pas publiés. L'incident est en cours de résolution</i>". CovidTracker sera à jour dès que ces données seront de nouveau disponibles.
 </div>
+-->
