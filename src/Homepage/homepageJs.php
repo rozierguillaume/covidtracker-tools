afec7b101133d22fda700d627c9a8daf7c833f7f
rozierguillaume <guillaume.rozier73@gmail.com>
2021-09-28 21:52:17 +0200

    test

--- src/Homepage/homepageJs.php
+++ src/Homepage/homepageJs.php
@@ -8,7 +8,7 @@
 
     //window.alert("Santé publique France a publié des données incomplètes et erronées ce soir. Nous les avons contactés. CovidTracker sera de nouveau à jour dès leur correction.");
 
-    fetch('https://raw.githubusercontent.com/rozierguillaume/covid-19/master/data/france/stats/objectif_deconfinement.json')
+    fetch('https://data.covidtracker.fr/data/france/stats/objectif_deconfinement.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error("HTTP error " + response.status);
