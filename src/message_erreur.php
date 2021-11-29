1dd34a7abd88d4c5925ed676bdc62fe420b1f1ad
rozierguillaume <guillaume.rozier73@gmail.com>
2021-11-29 21:17:35 +0100

    suppression message erreur

--- src/message_erreur.php
+++ src/message_erreur.php
@@ -1,4 +1,4 @@
-<div class="alert alert-warning" role="alert">
+<!--<div class="alert alert-warning" role="alert">
     <i>05 novembre •</i><b> Jours fériés : baisse artificielle des cas</b><br>
     Les données épidémiques (nombre de cas, nombre de tests) sont sous évaluées en raison de la présence de jours fériés (1er et 11 novembre). Les données hospitalières sont faiblement impactées.
-</div>
+</div>-->
