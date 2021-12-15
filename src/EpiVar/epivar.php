4228d475b14d6c32f35f6cafc09bbcb5b7feee50
rozierguillaume <guillaume.rozier73@gmail.com>
2021-12-15 18:15:54 +0100

    update

--- src/EpiVar/epivar.php
+++ src/EpiVar/epivar.php
@@ -89,6 +89,23 @@ p {
     <br>
     <h2>Résultats du modèle</h2>
 
+    <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+  <li class="nav-item" role="presentation">
+    <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</button>
+  </li>
+</ul>
+<div class="tab-content" id="pills-tabContent">
+  <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">...</div>
+  <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
+  <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
+</div>
+
     <div style="border-radius: 15px; border: 1px solid grey; padding: 5px; max-width: 950px;"><img src="https://raw.githubusercontent.com/CovidTrackerFr/forecast/main/output/model_booster_fit.png" style="width: 100%; max-width: 900px;"/></div>
     <br>
     <h2>Résultats de la semaine dernière</h2>
