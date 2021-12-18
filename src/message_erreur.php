06f290feb2d68067b7155274c349fec47ae7bed5
rozierguillaume <guillaume.rozier@telecomnancy.net>
2021-12-18 20:06:30 +0100

    message

--- src/message_erreur.php
+++ src/message_erreur.php
@@ -1,4 +1,5 @@
-<div class="alert alert-warning" role="alert">
+<!--<div class="alert alert-warning" role="alert">
     <i>17 décembre 2021</i><br>
     Les données Covid n’ont pas été mises à jour sur data.gouv ce soir. CovidTracker sera mis à jour automatiquement dès publication des données par Santé publique France. 
 </div>
+-->
