8cf5e05a7254de0808040205631d185e643c8754
Guillaume Rozier <guillaume.rozier@telecomnancy.net>
2022-01-04 20:58:04 +0100

    Update message_erreur.php

--- src/message_erreur.php
+++ src/message_erreur.php
@@ -1,5 +1,6 @@
-<!--<div class="alert alert-warning" role="alert">
-    <i>17 décembre 2021</i><br>
-    Les données Covid n’ont pas été mises à jour sur data.gouv ce soir. CovidTracker sera mis à jour automatiquement dès publication des données par Santé publique France. 
+<div class="alert alert-warning" role="alert">
+    <i>03 janvier 2022</i><br>
+    L'agence Santé publique France suspend temporairement la publication
+des indicateurs épidémiologiques à l'échelle départe-
+mental et infra-départementale en raison d'écarts constatés avec les données locales. Les données des départements et communautés de communes ne sont donc pas à jour sur CovidTracker.  
 </div>
--->
