958860d1a755d15fbc86d868d2f3d420f09202b5
Guillaume Rozier <guillaume.rozier@telecomnancy.net>
2022-01-04 21:00:27 +0100

    Update message_erreur.php

--- src/message_erreur.php
+++ src/message_erreur.php
@@ -1,6 +1,5 @@
 <div class="alert alert-warning" role="alert">
     <i>03 janvier 2022</i><br>
     L'agence Santé publique France suspend temporairement la publication
-des indicateurs épidémiologiques à l'échelle départe-
-mental et infra-départementale en raison d'écarts constatés avec les données locales. Les données des départements et communautés de communes ne sont donc pas à jour sur CovidTracker.  
+des indicateurs épidémiologiques à l'échelle départementale et infra-départementale en raison d'écarts constatés avec les données locales. Les graphiques et indicateurs au niveau département ne sont donc pas à jour sur CovidTracker.  
 </div>
