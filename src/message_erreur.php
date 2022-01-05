9c7b8409c7af1fed1727e5f839fcc11fd3b147eb
rozierguillaume <guillaume.rozier73@gmail.com>
2022-01-05 22:15:32 +0100

    update

--- src/message_erreur.php
+++ src/message_erreur.php
@@ -1,5 +1,6 @@
 <div class="alert alert-warning" role="alert">
-    <i>03 janvier 2022</i><br>
-    L'agence Santé publique France suspend temporairement la publication
-des indicateurs épidémiologiques à l'échelle départementale et infra-départementale en raison d'écarts constatés avec les données locales. Les graphiques et indicateurs au niveau département ne sont donc pas à jour sur CovidTracker.  
+    <i>⚠️ 03 janvier 2022</i><br>
+    L'agence Santé publique France suspend temporairement la publication des indicateurs épidémiologiques à l'échelle départementale et infra-départementale en raison d'écarts constatés avec les données locales.<br>
+    Les graphiques et indicateurs au niveau département ne sont donc pas à jour sur CovidTracker, les dernières données datant du 24/12.<br>
+    Les graphiques au niveau national sont à jour.  
 </div>
