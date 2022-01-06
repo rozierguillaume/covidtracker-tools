aaabc1d54341cb3b33fc0d70ac9fc8ff56d8dc1f
rozierguillaume <guillaume.rozier73@gmail.com>
2022-01-06 17:48:38 +0100

    updates

--- src/Homepage/enUnCoupDOeil.php
+++ src/Homepage/enUnCoupDOeil.php
@@ -61,7 +61,7 @@ Mise à jour : <span id="date_update_coup_doeil2">-/-</span>
                         <span id="cas_moyen_quotidien" style="font-size: 20px;">--</span> cas</b>
                     positifs au Covid19 <span id="cas_p2"></span>chaque jour,
                     <span id="croissance_cas" class="taux_croissance_stable">--</span>
-                    par rapport à la semaine dernière <span id="type_jour"></span>. <a href="https://covidtracker.fr/dashboard-depistage">Dépistage et cas ▸</a>
+                    par rapport à la semaine dernière <span id="type_jour"></span>. <a href="https://covidtracker.fr/france#depistage">Dépistage et cas ▸</a>
                 </p>
 
                 <div>
@@ -106,7 +106,7 @@ Mise à jour : <span id="date_update_coup_doeil2">-/-</span>
 
                 <p>Il y a en moyenne <b><span id="adm-hospit-value" style="font-size: 20px;">--</span>
                          <span id="typePersonnesAdm">admissions en soins critiques</span></b> pour Covid19 chaque jour,
-                    <span id="croissance-adm-hospit" class="taux_croissance_stable">--</span> par rapport à la semaine dernière.<br><a href="https://covidtracker.fr/dashboard-reanimations">Soins critiques ▸</a> &nbsp; <a href="https://covidtracker.fr/dashboard-hospitalisations">Hospitalisations ▸</a><br>
+                    <span id="croissance-adm-hospit" class="taux_croissance_stable">--</span> par rapport à la semaine dernière.<br><a href="https://covidtracker.fr/france#reanimations">Soins critiques ▸</a> &nbsp; <a href="https://covidtracker.fr/france#hospitalisations">Hospitalisations ▸</a><br>
                 </p>
                 <div>
                     <canvas id="admHospitChart" style="margin-top:20px; max-height: 700px; max-width: 900px;"></canvas>
@@ -147,7 +147,7 @@ Mise à jour : <span id="date_update_coup_doeil2">-/-</span>
 
                 <p>Il y a actuellement <b><span id="reanimations" style="font-size: 20px;">--</span>
                         personnes <span id="typePersonnes">en soins critiques</span></b> pour Covid19,
-                    <span id="croissance_rea" class="taux_croissance_stable">--</span> par rapport à la semaine dernière.<br><a href="https://covidtracker.fr/dashboard-reanimations">Soins critiques ▸</a> &nbsp; <a href="https://covidtracker.fr/dashboard-hospitalisations">Hospitalisations ▸</a><br>
+                    <span id="croissance_rea" class="taux_croissance_stable">--</span> par rapport à la semaine dernière.<br><a href="https://covidtracker.fr/france#reanimations">Soins critiques ▸</a> &nbsp; <a href="https://covidtracker.fr/france#hospitalisations">Hospitalisations ▸</a><br>
                 </p>
                 <div>
                     <canvas id="barChart" style="margin-top:20px; max-height: 700px; max-width: 900px;"></canvas>
