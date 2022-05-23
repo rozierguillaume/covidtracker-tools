2800e708a95b6a65a880024d60383f4a45b13d67
rozierguillaume <guillaume.rozier73@gmail.com>
2022-05-23 14:03:09 +0200

    message erreur

--- src/message_erreur.php
+++ src/message_erreur.php
@@ -1,4 +1,7 @@
 <div class="alert alert-warning" role="alert">
+    <i>23 mai</i><br>
+    La plupart des graphiques sont désormais de nouveau à jour. 
+    <br>
     <i>22 mai 2022</i><br>
     En raison de changements importants dans le format des données opéré par Santé publique France, CovidTracker n'est plus à jour depuis le 17 mai. Nous sommes en train d'adapter les traitements aux nouveaux formats, et espérons un correctif dans les plus brefs délais. Veuillez nous excuser pour la gêne occasionée.
 </div>
